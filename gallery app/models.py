28d151d187af4ea26e89ad24ee3d5c528fca6bad
kilonzijnr <vkilonzi05@gmail.com>
2021-10-12 09:59:13 +0300

    created images class for handling image operations

--- gallery app/models.py
+++ gallery app/models.py
@@ -27,3 +27,42 @@ class Category(models.Model):
     def delete_caategory(self):
         self.delete()
 
+
+class Image(models.Model):
+    image = models.ImageField(upload_to='photos/', default=None)
+    name = models.CharField(max_length= 30)
+    description = models.TextField()
+    location = models.ForeignKey(
+        Location, on_delete=models.CASCADE, default=None)
+    category = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return self.name
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    @classmethod
+    def get_image_by_id(cls, id):
+        image = cls.objects.filter(id_icontains = id)
+        return image
+
+    @classmethod
+    def get_image_by_category(cls, category):
+        image = cls.objects.filter(category_icontains=category)
+        return image
+
+    @classmethod
+    def get_image_by_location(cls, location):
+        image = cls.objects.filter(location_icontains=location)
+        return image
+
+    @classmethod
+    def search_by_name(cls, search_term):
+        images = cls.objects.filter(name_icontains= search_term)
+        return images
+
+
