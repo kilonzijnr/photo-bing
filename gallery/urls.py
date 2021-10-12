08829f43cadd9fcf7f332fa32edebc42e0f5bb9c
kilonzijnr <vkilonzi05@gmail.com>
2021-10-12 10:57:29 +0300

    changes urls

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,10 +1,12 @@
 from django.urls.conf import path
 from . import views
 from django.conf import settings
-
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.photos, name='photos'),
     path('search/', views.search_images, name="search_images"),
     path('category/<category>', views.view_category, name='view_category'),
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- tribune/settings.py
+++ tribune/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bootstrap3',
+    'photos'
+    
 ]
 
 MIDDLEWARE = [
@@ -64,6 +66,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
