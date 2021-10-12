421511583217ecfeb275b13d4c746b5bcd887b40
kilonzijnr <vkilonzi05@gmail.com>
2021-10-12 18:17:05 +0300

    configured app settings

--- tribune/settings.py
+++ tribune/settings.py
@@ -33,13 +33,13 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'gallery',
+    'django_bootstrap5', 
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'bootstrap3',    
+    'django.contrib.staticfiles',   
 ]
 
 MIDDLEWARE = [
