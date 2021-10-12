6342d32b8b2ca8ab1129a2ee72eb5b91491ef06b
kilonzijnr <vkilonzi05@gmail.com>
2021-10-12 18:11:22 +0300

    added URLS's for location and categories

--- gallery/urls.py
+++ gallery/urls.py
@@ -4,9 +4,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.photos, name='gallery'),
+    path('', views.photos, name='photos'),
     path('search/', views.search_images, name="search_images"),
     path('category/<category>', views.view_category, name='view_category'),
+    path('location/<location>', views.view_location, name='view_location'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
