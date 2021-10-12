0e8d9255c5192529094fd8d0b8854f9ae0a4c190
kilonzijnr <vkilonzi05@gmail.com>
2021-10-12 09:23:47 +0300

    created model classes for location and category

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,29 @@
+from os import name
 from django.db import models
 
 # Create your models here.
+
+class Location(models.Model):
+    name = models.CharField(max_length= 30)
+
+    def __str__(self):
+        return self.name
+
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
+
+class Category(models.Model):
+    name = models.CharField(max_length= 30)
+
+    def __str__(self):
+        return self.name
+
+    def save_category(self):
+        self.save()
+
+    def delete_caategory(self):
+        self.delete()
+
